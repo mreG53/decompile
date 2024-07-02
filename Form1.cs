f2b28c1b5f19da03c8846ca0bdae0d99843d3849
Emre Gümüş <o214001063@giresun.edu.tr>
2024-07-02 13:36:31 +0300

    fupdate

--- Form1.cs
+++ Form1.cs
@@ -18,12 +18,6 @@ namespace WindowsFormsApp1
 
 		private OpenFileDialog openFileDialog1;
 
-		private Button filePath;
-
-		private TextBox pathText;
-
-		private Button deleteBtn;
-
 		private Button recentDelete;
 
 		private Label label1;
@@ -48,54 +42,6 @@ namespace WindowsFormsApp1
 			return str;
 		}
 
-		private void deleteBtn_Click(object sender, EventArgs e)
-		{
-			this.progressBar1.Value = 0;
-			this.timer1.Enabled = true;
-			if (this.pathText.Text != "")
-			{
-				string text = this.pathText.Text;
-				string[] files = Directory.GetFiles(text);
-				string[] directories = Directory.GetDirectories(text);
-				string[] strArrays = files;
-				for (int i = 0; i < (int)strArrays.Length; i++)
-				{
-					string str = strArrays[i];
-					try
-					{
-						File.Delete(str);
-					}
-					catch (Exception exception1)
-					{
-						Exception exception = exception1;
-						this.label53.Visible = true;
-						this.label53.MaximumSize = new System.Drawing.Size(275, 20);
-						this.label53.Text = string.Concat("Dosyalar silindi, Sistemde çalışan dosyalar silinmedi. Dosya Adı : ", str, "Hata : ", exception.Message);
-					}
-				}
-				string[] strArrays1 = directories;
-				for (int j = 0; j < (int)strArrays1.Length; j++)
-				{
-					string str1 = strArrays1[j];
-					try
-					{
-						Directory.Delete(str1, true);
-					}
-					catch (Exception exception3)
-					{
-						Exception exception2 = exception3;
-						this.label53.Visible = true;
-						this.label53.MaximumSize = new System.Drawing.Size(275, 20);
-						this.label53.Text = string.Concat("Dosyalar silindi, Sistemde çalışan dosyalar silinmedi. Dosya Adı : ", str1, "Hata : ", exception2.Message);
-					}
-				}
-				MessageBox.Show("İşlem başarılı.", "Recent Files", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-			}
-			else
-			{
-				MessageBox.Show("Lütfen dosya yolu seçiniz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-			}
-		}
 
 		protected override void Dispose(bool disposing)
 		{
@@ -104,14 +50,6 @@ namespace WindowsFormsApp1
 				this.components.Dispose();
 			}
 			base.Dispose(disposing);
-		}
-
-		private void filePath_Click(object sender, EventArgs e)
-		{
-			FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-			folderBrowserDialog.ShowDialog();
-			this.pathText.Text = folderBrowserDialog.SelectedPath.ToString();
-			folderBrowserDialog.SelectedPath.ToString();
 		}
 
 		public static long FolderSizeCalculation(string yol)
@@ -127,107 +65,101 @@ namespace WindowsFormsApp1
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			if (MessageBox.Show("Dosya yolu el ile değiştirilsin mi? İlk defa çalıştırıyorsanız 'Hayır' seçeneğini işaretleyin.", "Path", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) != System.Windows.Forms.DialogResult.Yes)
-			{
-				this.pathText.ReadOnly = true;
-			}
-			else
-			{
-				this.pathText.ReadOnly = false;
-			}
 			this.label53.Visible = false;
 			this.label53.ForeColor = Color.Red;
 			this.label1.MaximumSize = new System.Drawing.Size(350, 500);
 			this.label1.ForeColor = Color.Green;
-			this.label1.Text = "Windows çöp dosyaları; Recent, Temporary Files ve Prefetch olarak adlandırılır. Bu dosyaları silmek bilgisayarınızda yer açma, az da olsa hızlandırma ve bilgisayarınıza önem vermenizi sağlar. Soldaki 'Temp sil' ve 'Recent Sil' butonları ile bilgisayarınızdaki tüm çöp dosyalarını silebilirsiniz.";
+			this.label1.Text = "%0";
 			base.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
 		}
 
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			ComponentResourceManager componentResourceManager = new ComponentResourceManager(typeof(Form1));
-			this.tempSil = new Button();
-			this.label53 = new Label();
-			this.openFileDialog1 = new OpenFileDialog();
-			this.filePath = new Button();
-			this.pathText = new TextBox();
-			this.deleteBtn = new Button();
-			this.recentDelete = new Button();
-			this.label1 = new Label();
-			this.progressBar1 = new ProgressBar();
-			this.timer1 = new Timer(this.components);
-			base.SuspendLayout();
-			this.tempSil.Location = new Point(15, 7);
-			this.tempSil.Name = "tempSil";
-			this.tempSil.Size = new System.Drawing.Size(169, 65);
-			this.tempSil.TabIndex = 0;
-			this.tempSil.Text = "Temp Sil";
-			this.tempSil.UseVisualStyleBackColor = true;
-			this.tempSil.Click += new EventHandler(this.tempSil_Click);
-			this.label53.AutoSize = true;
-			this.label53.Location = new Point(12, 75);
-			this.label53.Name = "label53";
-			this.label53.Size = new System.Drawing.Size(24, 13);
-			this.label53.TabIndex = 1;
-			this.label53.Text = "text";
-			this.openFileDialog1.FileName = "openFileDialog1";
-			this.filePath.Location = new Point(499, 7);
-			this.filePath.Name = "filePath";
-			this.filePath.Size = new System.Drawing.Size(75, 23);
-			this.filePath.TabIndex = 3;
-			this.filePath.Text = "Dosya Seç";
-			this.filePath.UseVisualStyleBackColor = true;
-			this.filePath.Click += new EventHandler(this.filePath_Click);
-			this.pathText.BackColor = SystemColors.WindowFrame;
-			this.pathText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25f, FontStyle.Bold, GraphicsUnit.Point, 162);
-			this.pathText.ForeColor = Color.Red;
-			this.pathText.Location = new Point(205, 7);
-			this.pathText.Name = "pathText";
-			this.pathText.Size = new System.Drawing.Size(288, 20);
-			this.pathText.TabIndex = 4;
-			this.deleteBtn.Location = new Point(499, 36);
-			this.deleteBtn.Name = "deleteBtn";
-			this.deleteBtn.Size = new System.Drawing.Size(75, 23);
-			this.deleteBtn.TabIndex = 5;
-			this.deleteBtn.Text = "Sil";
-			this.deleteBtn.UseVisualStyleBackColor = true;
-			this.deleteBtn.Click += new EventHandler(this.deleteBtn_Click);
-			this.recentDelete.Location = new Point(15, 91);
-			this.recentDelete.Name = "recentDelete";
-			this.recentDelete.Size = new System.Drawing.Size(169, 71);
-			this.recentDelete.TabIndex = 6;
-			this.recentDelete.Text = "Recent Sil";
-			this.recentDelete.UseVisualStyleBackColor = true;
-			this.recentDelete.Click += new EventHandler(this.recentDelete_Click_1);
-			this.label1.AutoSize = true;
-			this.label1.Location = new Point(202, 91);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(35, 13);
-			this.label1.TabIndex = 7;
-			this.label1.Text = "label1";
-			this.progressBar1.Location = new Point(205, 36);
-			this.progressBar1.Name = "progressBar1";
-			this.progressBar1.Size = new System.Drawing.Size(288, 23);
-			this.progressBar1.TabIndex = 8;
-			this.timer1.Tick += new EventHandler(this.timer1_Tick);
-			base.AutoScaleDimensions = new SizeF(6f, 13f);
-			base.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			base.ClientSize = new System.Drawing.Size(588, 167);
-			base.Controls.Add(this.progressBar1);
-			base.Controls.Add(this.label1);
-			base.Controls.Add(this.recentDelete);
-			base.Controls.Add(this.deleteBtn);
-			base.Controls.Add(this.pathText);
-			base.Controls.Add(this.filePath);
-			base.Controls.Add(this.label53);
-			base.Controls.Add(this.tempSil);
-			base.Icon = (System.Drawing.Icon)componentResourceManager.GetObject("$this.Icon");
-			base.Name = "Form1";
-			this.Text = "Delete Windows Trash Files and Directories";
-			base.Load += new EventHandler(this.Form1_Load);
-			base.ResumeLayout(false);
-			base.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tempSil = new System.Windows.Forms.Button();
+            this.label53 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.recentDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tempSil
+            // 
+            this.tempSil.Location = new System.Drawing.Point(40, 7);
+            this.tempSil.Name = "tempSil";
+            this.tempSil.Size = new System.Drawing.Size(169, 65);
+            this.tempSil.TabIndex = 0;
+            this.tempSil.Text = "Temp Sil";
+            this.tempSil.UseVisualStyleBackColor = true;
+            this.tempSil.Click += new System.EventHandler(this.tempSil_Click);
+            // 
+            // label53
+            // 
+            this.label53.AutoSize = true;
+            this.label53.Location = new System.Drawing.Point(1, 197);
+            this.label53.Name = "label53";
+            this.label53.Size = new System.Drawing.Size(24, 13);
+            this.label53.TabIndex = 1;
+            this.label53.Text = "text";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // recentDelete
+            // 
+            this.recentDelete.Location = new System.Drawing.Point(40, 120);
+            this.recentDelete.Name = "recentDelete";
+            this.recentDelete.Size = new System.Drawing.Size(169, 71);
+            this.recentDelete.TabIndex = 6;
+            this.recentDelete.Text = "Recent Sil";
+            this.recentDelete.UseVisualStyleBackColor = true;
+            this.recentDelete.Click += new System.EventHandler(this.recentDelete_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(40, 91);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(169, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 213);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.recentDelete);
+            this.Controls.Add(this.label53);
+            this.Controls.Add(this.tempSil);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DWTFD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
 		private void recentDelete_Click_1(object sender, EventArgs e)
@@ -247,7 +179,7 @@ namespace WindowsFormsApp1
 			string tempPath = Path.GetTempPath();
 			long ınt64 = Form1.FolderSizeCalculation(str);
 			long ınt641 = Form1.FolderSizeCalculation(tempPath);
-			MessageBox.Show(string.Concat("Bulunan dosya boyutu :", Form1.BytesToString(ınt64 + ınt641)), "Dosya Boyutu", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+			
 			string[] files = Directory.GetFiles(str);
 			string[] directories = Directory.GetDirectories(str);
 			string[] strArrays = Directory.GetFiles(tempPath);
@@ -351,8 +283,8 @@ namespace WindowsFormsApp1
 					this.label53.Text = string.Concat("Dosyalar silindi, Sistemde çalışan dosyalar silinmedi. Dosya Adı : ", str7, "Hata : ", exception10.Message);
 				}
 			}
-			MessageBox.Show("İşlem başarılı.", "Temporary Files", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-		}
+            MessageBox.Show(string.Concat("Bulunan dosya boyutu :", Form1.BytesToString(ınt64 + ınt641)), "Dosya Boyutu", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
 
 		private void timer1_Tick(object sender, EventArgs e)
 		{
@@ -388,5 +320,5 @@ namespace WindowsFormsApp1
 				PathW = 3
 			}
 		}
-	}
+    }
 }
